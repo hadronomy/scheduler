af311c8f7e783061e4c4f6c6da9634b237e8b5f2
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-09-10 17:22:22 +0100

    feat: use `@effect/cli`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { Command } from '@effect/cli';
+import { BunContext } from '@effect/platform-bun';
 import { Cause, Effect, Exit, Logger } from 'effect';
+
 import { AppConfig, ConfigLive } from '~/config';
 
 const program = Effect.gen(function* () {
@@ -8,8 +11,16 @@ const program = Effect.gen(function* () {
   yield* Effect.log(`Found ${files.length} files in folder`);
 });
 
-program
+const command = Command.make('schedule', {}, () => program);
+
+const cli = Command.run(command, {
+  name: 'scheduler',
+  version: 'v1.0.0',
+});
+
+cli(process.argv)
   .pipe(
+    Effect.provide(BunContext.layer),
     Effect.provide(ConfigLive),
     Effect.provide(Logger.pretty),
     Effect.runPromiseExit,
