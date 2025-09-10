a61fafe797a1f6d71589475947060097331e319e
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-09-10 15:28:29 +0100

    refactor: use effect service instead of context

--- src/services/ocr.ts
+++ src/services/ocr.ts
@@ -1,12 +1,13 @@
-import { Context, Effect } from 'effect';
-import { type IanaMediaType } from '../generated/iana-media-types';
+import { Effect } from 'effect';
+import type { IanaMediaType } from '../generated/iana-media-types';
 
-export class ScheduleOCR extends Context.Tag('ScheduleOCR')<
-  ScheduleOCR,
-  {
-    readonly extractFromBytes: (
-      input: Uint8Array,
-      mediaType: IanaMediaType,
-    ) => Effect.Effect<never, Error, never>;
-  }
->() {}
+export class ScheduleOCR extends Effect.Service<ScheduleOCR>()('scheduleocr', {
+  effect: Effect.gen(function* () {
+    function extractFromBytes(input: Uint8Array, mediaType: IanaMediaType) {
+      return Effect.succeed(`${input} is a ${mediaType}`);
+    }
+    return {
+      extractFromBytes,
+    } as const;
+  }),
+}) {}
